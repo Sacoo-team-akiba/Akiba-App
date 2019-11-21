f231a3f08ff289248954255892a12835332b6b66
Sammie <mugishasamuel956@gmail.com>
2019-11-21 13:44:12 +0300

    adding files

--- app/src/main/java/com/example/akiba/ui/BorrowLoan/BorrowLoanFragment.kt
+++ app/src/main/java/com/example/akiba/ui/BorrowLoan/BorrowLoanFragment.kt
@@ -4,28 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.akiba.R
 
 class BorrowLoanFragment : Fragment() {
-
-    private lateinit var borrowLoanViewModel: BorrowLoanViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        borrowLoanViewModel =
-            ViewModelProviders.of(this).get(BorrowLoanViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_borrowloan, container, false)
-        val textView: TextView = root.findViewById(R.id.text_borrowloan)
-        borrowLoanViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
