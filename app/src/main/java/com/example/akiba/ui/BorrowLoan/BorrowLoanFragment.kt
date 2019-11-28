6c4ac5970fa429adb65a68b26c7d6428f4ceba02
muni university <ainembabazilawrence75@gmail.com>
2019-11-28 13:53:08 +0300

    adding files

--- app/src/main/java/com/example/akiba/ui/BorrowLoan/BorrowLoanFragment.kt
+++ app/src/main/java/com/example/akiba/ui/BorrowLoan/BorrowLoanFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import androidx.fragment.app.Fragment
 import com.example.akiba.R
+import kotlinx.android.synthetic.main.fragment_borrowloan.*
+import kotlinx.android.synthetic.main.fragment_borrowloan.view.*
+import kotlinx.android.synthetic.main.fragment_payloan.*
 
 class BorrowLoanFragment : Fragment() {
 
@@ -13,8 +17,14 @@ class BorrowLoanFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+
         val root = inflater.inflate(R.layout.fragment_borrowloan, container, false)
+        root.numberPicker1.minValue = 0
+        root.numberPicker1.maxValue = 31
         return root
     }
+
+
 }
